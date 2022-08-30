ebcfdbc7e72ad470fe9e9da55b42880a9b58327e
IT-ENGINEERING <IT-ENGINEERING@DESKTOP-K06LIJQ>
2022-08-30 11:51:59 +0200

    POPULATE PERSONNEL IN FLOWLAYOUT DONE

--- Paie personnel/Paie personnel/FrmPersonnel.cs
+++ Paie personnel/Paie personnel/FrmPersonnel.cs
@@ -1,4 +1,6 @@
-﻿using Paie_personnel.Common.UserControl;
+﻿using MySql.Data.MySqlClient;
+using Paie_personnel.Common.Helper;
+using Paie_personnel.Common.UserControl;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -16,6 +18,7 @@ namespace Paie_personnel
         public FrmPersonnel()
         {
             InitializeComponent();
+            PopulateFlowLayout();
         }
 
         private void ListePersonnel_Paint(object sender, PaintEventArgs e)
@@ -26,6 +29,40 @@ namespace Paie_personnel
         private void BtnAjouter_Click(object sender, EventArgs e)
         {
             new FrmAjouterPersonnel().ShowDialog();
+        }
+
+
+        private void PopulateFlowLayout()
+        {
+            this.Cursor = Cursors.WaitCursor;
+
+            string sql = "select nom,fonction from personnel";
+            
+            using (MySqlCommand cmd =new MySqlCommand(sql, Connexion.Con))
+            {
+                using(MySqlDataAdapter da=new MySqlDataAdapter(cmd))
+                {
+                    DataTable table = new DataTable();
+
+                    da.Fill(table);
+
+                    
+                    foreach(DataRow dr in table.Rows)
+                    {
+                        Personnel personnel = new Personnel()
+                        {
+                            Nom = dr[0].ToString(),
+                            Fonction = dr[1].ToString(),
+                        };
+
+                        ListePersonnel.Controls.Add(personnel);
+                    }
+
+                }
+            }
+
+
+            this.Cursor = Cursors.Default;
         }
     }
 }
